0d5d9ab053ce67bfad116047990a7af81e939288
Rafhaela Marques <rafaelamarques_z@hotmail.com>
2024-05-27 22:27:34 -0300

    registrado hive adapter

--- lib/services/hive_adapters.dart
+++ lib/services/hive_adapters.dart
@@ -1,3 +1,8 @@
+import 'package:hive/hive.dart';
+import 'package:showtrack/data/model/show.dart';
+
 class HiveAdapters {
-  static void init() {}
+  static void init() {
+    Hive.registerAdapter(ShowAdapter());
+  }
 }
