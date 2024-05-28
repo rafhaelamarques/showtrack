105e6ed39c51d9a99e4e0dd41a0fa80ad101b3d5
Rafhaela Marques <rafaelamarques_z@hotmail.com>
2024-05-27 22:06:59 -0300

    ajustes na main

---
+++ lib/core/application.dart
@@ -0,0 +1,24 @@
+import 'package:hive/hive.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:showtrack/data/webapi/dio/dio_tv_maze_creator.dart';
+import 'package:showtrack/services/hive_adapters.dart';
+
+class Application {
+  /// Inicialização das definições do app
+  static Future initialize() async {
+    await _hiveInit();
+    await _dioInit();
+  }
+
+  static Future<void> _hiveInit() async {
+    final directory = await getApplicationDocumentsDirectory();
+    Hive.init(directory.path);
+    HiveAdapters.init();
+  }
+
+  static Future<void> _dioInit() async {
+    // TV Maze API
+    const baseUrl = 'https://api.tvmaze.com';
+    await DioTvMazeCreator().initialize(baseUrl);
+  }
+}
