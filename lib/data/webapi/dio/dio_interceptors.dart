bf7aa274378940c9783065fffce0e093d579a75d
Rafhaela Marques <rafaelamarques_z@hotmail.com>
2024-05-29 18:15:01 -0300

    alterado interceptors

--- lib/data/webapi/dio/dio_interceptors.dart
+++ lib/data/webapi/dio/dio_interceptors.dart
@@ -5,7 +5,8 @@ class DioInterceptors extends Interceptor {
   @override
   Future onRequest(
       RequestOptions options, RequestInterceptorHandler handler) async {
-    debugPrint('REQUEST[${options.method}] => PATH: ${options.path}');
+    debugPrint(
+        'REQUEST[${options.method}] => PATH: ${options.path}/${options.queryParameters['q']}');
 
     return super.onRequest(options, handler);
   }
@@ -14,15 +15,14 @@ class DioInterceptors extends Interceptor {
   Future onResponse(
       Response response, ResponseInterceptorHandler handler) async {
     debugPrint(
-        'RESPONSE[${response.statusCode}] => PATH: ${response.requestOptions.path}');
+        'RESPONSE[${response.statusCode}] => PATH: ${response.requestOptions.path}/${response.requestOptions.queryParameters['q']}');
 
     return super.onResponse(response, handler);
   }
 
   @override
   Future onError(DioException err, ErrorInterceptorHandler handler) async {
-    debugPrint(
-        'ERROR[${err.response?.statusCode}] => PATH: ${err.requestOptions.path}');
+    debugPrint('ERROR[${err.response?.statusCode}] => MESSAGE: ${err.message}');
 
     return super.onError(err, handler);
   }
